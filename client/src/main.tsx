efa33e89eeee574eb1ce0bea565c19399d148fd0
IMChamberlain <ISAACMCCHAMBERLAIN@GMAIL.COM>
2024-11-26 20:57:32 -0700

    Some random stuff. Updated index.html with logo and name, started the main.tsx, updated ErrorPage.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,8 +1,31 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css'
 
-createRoot(document.getElementById('root')!).render(
-    <App />
-)
+import App from './App.tsx';
+import ErrorPage from './pages/ErrorPage.tsx';
+import Home from './pages/Home.tsx';
+import Hangman from './pages/Hangman.tsx';
+
+const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <App />,
+      errorElement: <ErrorPage />,
+      children: [
+        {
+          index: true,
+          element: <Home />
+        },  
+        {
+          path: "/Hangman",
+          element: <Hangman />
+        },  
+      ]
+    }
+  ])
+  
+  const rootElement = document.getElementById('root');
+  if (rootElement) {
+    ReactDOM.createRoot(rootElement).render(<RouterProvider router={router} />);
+  }
