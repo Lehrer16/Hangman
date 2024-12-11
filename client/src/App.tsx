74c3111538c86e4f530de87f0824a982613a6565
Lehrer16 <jesseisthegreatest@gmail.com>
2024-12-10 20:56:14 -0600

    deleted usestate

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+
 import { Outlet } from 'react-router-dom';
 import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
